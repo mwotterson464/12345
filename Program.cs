82cbe99f9c9584caeba56e4bdbdcfba7612ed93e
mwotterson464 <mwottersom@inbox.ru>
2020-10-02 08:50:13 +0000

    reklama commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReklamnnoeAgency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
